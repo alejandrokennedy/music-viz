4411561785dc44b3b30b862f905b3167df7ecc2f
patriciatiffany <30840301+patriciatiffany@users.noreply.github.com>
2021-02-12 21:15:05 -0800

    Made progress on tensorflow model (normalized data to [0,1] by dividing by midi scale range, store predictions in array)

--- demo.js
+++ demo.js
@@ -1,7 +1,12 @@
 async function getMidi() {
 
+    // 2021/02/12: 
+    // so far: got a model to predict a note for a pair of notes
+    // next: loop recursively over pairs + predictions to get a longer string of notes
+    // next next: randomness
+
     const midi = await Midi.fromUrl("./data/musicnet_midis/Bach/2186_vs6_1.mid")
-    console.log(midi)
+    // console.log(midi)
     
     const ourNotes = []
     const nextNotes = []
@@ -24,7 +29,6 @@ async function getMidi() {
             // ourNotes.push(justNotes)
             ourNotes.push(justNotesPairs)
             nextNotes.push(nextNote)
-
         }
     })
 
@@ -32,18 +36,28 @@ async function getMidi() {
     ourNotesForModel.pop() // Remove the last pair
     const nextNotesForModel = nextNotes[0]
     nextNotesForModel.pop()
-    console.log('ourNotesForModel', ourNotesForModel)
-    console.log('nextNotesForModel', nextNotesForModel)
-    const x_train = tf.tensor(ourNotesForModel)
-    const y_train = tf.tensor(nextNotesForModel)
+    // console.log('ourNotesForModel', ourNotesForModel)
+    // console.log('nextNotesForModel', nextNotesForModel)
+
+    // const x_train = tf.tensor(ourNotesForModel) //.map(d => d/127)
+    // const y_train = tf.tensor(nextNotesForModel.map(d => [d])) 
+    
+    const x_train_norm = ourNotesForModel.map(d => [d[0] / 127, d[1] / 127])
+    const y_train_norm = nextNotesForModel.map(d => d / 127)
+
+    const x_train = tf.tensor(x_train_norm) //ourNotesForModel) //.map(d => d/127)
+    const y_train = tf.tensor(y_train_norm) // nextNotesForModel.map(d => [d])) 
+    
+    console.log('x_train_norm', x_train_norm)
+    // console.log(y_train_norm)
 
     // Make the model with TensorFlow
     const model = tf.sequential(); // A sequential model is appropriate for a plain stack of layers where each layer has exactly one input tensor and one output tensor.
 
     // Config for layer in objects to feed into tf
     const config_hidden = {
-        // inputShape: [3], // Corresponds to dimension of x_train, x_test below
         inputShape: [2], // Corresponds to dimension of x_train, x_test below
+        // inputShape: [3], // Corresponds to dimension of x_train, x_test below
         activation: 'sigmoid',
         units: 4 // <- what is this?
     }
@@ -57,11 +71,14 @@ async function getMidi() {
     // Defining the hidden and output layer
     const hidden = tf.layers.dense(config_hidden);
     const output = tf.layers.dense(config_output);
+    
     // Adding layers to model
     model.add(hidden);
     model.add(output);
+    
     // Define an optimizer
     const optimize = tf.train.sgd(0.1);
+    
     // Config for model
     const config = {
         optimizer: optimize,
@@ -85,15 +102,35 @@ async function getMidi() {
     //     [0.5, 0.5]
     // ])
     // Dummy testing data
+    
     const x_test = tf.tensor([
         // [0.7, 0.1, 0.9]
-        [88, 92]
+        // [88, 92]
+        [0.6929133858267716, 0.7244094488188977]
     ])
+
+    resultsArr = []
+
     train_data().then(function () {
         console.log('Training is Complete');
-        console.log('Predictions :');
+        // console.log('Predictions :');
+        
+        model.predict(x_test);
         model.predict(x_test).print();
+
+        x_train_norm.forEach(d => {
+            resultsArr.push(model.predict(tf.tensor([d])).arraySync());
+        })
+
+        resultsArrJoy = resultsArr.map(d => d[0] * 127)
+
+        // console.log(model.predict(x_test));
+        // console.log('joy?', model.predict(x_test).arraySync());
+        console.log(resultsArr);
+        console.log(resultsArrJoy);
+
     })
+
     async function train_data() {
         for (let i = 0; i < 100; i++) {
             // The batch size is a number of samples processed before the model is updated.
@@ -106,7 +143,11 @@ async function getMidi() {
         }
     }
 
+    // model.predict(tf.tensor([[89,92]])).print()
 
+    // model.weights.forEach(w => {
+    //     console.log(w.name, w.shape);
+    //    });
 
 }
 
